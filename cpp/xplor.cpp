45c4602bbe7f05a759e9be974d7538a1cf3165c4
Ali Yahya <alive@replicant.com>
2015-03-06 12:45:06 -0800

    exploring c++ move semantics

--- cpp/xplor.cpp
+++ cpp/xplor.cpp
@@ -95,6 +95,113 @@ TEST(MoveSemantics, TypeSlicing) {
     ASSERT_EQ(13, ar.foo());
 }
 
+// @story
+struct D {
+    // constructor
+    explicit D(int a) : data(a) {}
+
+    // destructor
+    ~D() {
+        data = 0;
+    }
+
+    // copy constructor
+    D(const D& other) {
+        data = other.data;
+        ++copy_count;
+    }
+
+    // move constructor
+    D(D&& other) : D(0) { // calls standard constructor; initializes instance
+        swap(*this, other);
+        ++move_count;
+    }
+
+    // copy-and-swap idiom:
+    // http://stackoverflow.com/questions/3279543/what-is-the-copy-and-swap-idiom
+    //
+    // Advantages:
+    // * efficiency:
+    //   * eliminates need to check for self assignment: if (this != other)
+    //   * allows the compiler to optimize copy (pass-by-value);
+    //     guideline: let the compiler perform copies for you whenever possible
+    //
+    // * conciseness
+    //   * reduces code duplication: factors common bits into swap function;
+    //     move constructor and assignment operator thus become trivial.
+    //   * copy assignment operator and move assignment operator are fused into one
+    //
+    // * correctness
+    //   * strong exception guarantee
+    //
+    D& operator=(D d) {
+        // if the operand is an l-value, it is copied into `d` using the copy constructor
+        // if the operand is an r-value, it is moved into `d` using the move constructor
+        swap(*this, d);
+        return *this;
+    }
+
+    friend void swap(D& a, D& b) {
+        std::swap(a.data, b.data);
+    }
+
+    // unrelated: equality operator
+    friend bool operator==(const D& lhs, const D& rhs) {
+        return lhs.data == rhs.data;
+    }
+
+    int data;
+
+    static int copy_count;
+    static int move_count;
+};
+
+int D::copy_count = 0;
+int D::move_count = 0;
+
+TEST(MoveSemantics, CopyAndSwap) {
+    D::copy_count = 0;
+    D::move_count = 0;
+
+    D a(42);
+    D b(a);
+
+    EXPECT_EQ(1, D::copy_count);
+    EXPECT_EQ(0, D::move_count);
+    EXPECT_EQ(a, b);
+}
+
+TEST(MoveSemantics, MoveAndSwap) {
+    D::copy_count = 0;
+    D::move_count = 0;
+
+    D a(42);
+    D b(std::move(a));
+
+    EXPECT_EQ(0, a.data);
+    EXPECT_EQ(42, b.data);
+    EXPECT_EQ(0, D::copy_count);
+    EXPECT_EQ(1, D::move_count);
+}
+
+TEST(MoveSemantics, Assignment) {
+    D::copy_count = 0;
+    D::move_count = 0;
+
+    D a(42);
+    D b = a;
+
+    EXPECT_EQ(1, D::copy_count);
+    EXPECT_EQ(0, D::move_count);
+    EXPECT_EQ(a, b);
+
+    D c = std::move(a);
+    EXPECT_EQ(1, D::copy_count);
+    EXPECT_EQ(1, D::move_count);
+    EXPECT_EQ(0, a.data);
+    EXPECT_EQ(b, c);
+}
+
 } // namespace move_semantics
 
 
