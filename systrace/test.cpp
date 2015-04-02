45eead6227af4df2e2ea748fdc36f89b6a5becff
Ali Yahya <alive@replicant.com>
2015-04-02 11:37:48 -0700

    systrace test

---
+++ systrace/test.cpp
@@ -0,0 +1,37 @@
+
+ #define MAX_PATH 256
+#define _STR(x) #x
+#define STR(x) _STR(x)
+
+char *find_debugfs() {
+    static char debugfs[MAX_PATH+1];
+    static int debugfs_found;
+    char type[100];
+    FILE *fp;
+
+    if (debugfs_found)
+        return debugfs;
+
+    if ((fp = fopen("/proc/mounts","r")) == NULL)
+        return NULL;
+
+    while (fscanf(fp, "%*s %"
+          STR(MAX_PATH)
+          "s %99s %*s %*d %*d\n",
+          debugfs, type) == 2) {
+        if (strcmp(type, "debugfs") == 0)
+            break;
+    }
+    fclose(fp);
+
+    if (strcmp(type, "debugfs") != 0)
+        return NULL;
+
+    debugfs_found = 1;
+
+    return debugfs;
+}
+
+int main(int argc, char **argv) {
+    printf("%s\n", find_debugfs());
+}
